50bd8b1b97081c90a09f72fd8f5ff4df7f30405a
Magdiel JG <dany_ring@ciencias.unam.mx>
2020-04-20 14:58:11 -0500

    websockets

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -43,13 +43,21 @@ if (token) {
  * allows your team to easily build robust real-time web applications.
  */
 
-// import Echo from 'laravel-echo'
+import Echo from 'laravel-echo'
 
-// window.Pusher = require('pusher-js');
+window.Pusher = require('pusher-js');
 
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     encrypted: true
-// });
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: process.env.MIX_PUSHER_APP_KEY,
+    // cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+    // encrypted: true
+    wsHost: '127.0.0.1',
+    wsPort: 6001,
+    disableStats: true,
+});
+
+window.Echo.channel('home')
+.listen('NewMessage', (e) => {
+    console.log(e);
+});
