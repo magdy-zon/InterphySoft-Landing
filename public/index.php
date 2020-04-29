7e9290652955ecb58f401604d0becccb3832df46
Magdiel JG <dany_ring@ciencias.unam.mx>
2020-04-29 15:01:41 -0500

    websockets server

--- public/index.php
+++ public/index.php
@@ -9,9 +9,9 @@
 
 define('LARAVEL_START', microtime(true));
 
-// header("Access-Control-Allow-Origin: *");
-// header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
-// header("Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization");
 
 /*
 |--------------------------------------------------------------------------
